277dba0e36e1ffff4cb6c2ee90b2b394bf5badbe
datagnasker <datagnasker@proton.me>
2024-09-17 15:05:15 +0200

    setup gunicorn

--- bitbibliotek/__init__.py
+++ bitbibliotek/__init__.py
@@ -3,11 +3,15 @@ import uuid
 from flask import Flask
 
 app = Flask(__name__)
-app.config.update(
-    FLAG=os.getenv('FLAG', 'FLAG_PLACEHOLDER'),
-    USERNAME=os.getenv('NAME', 'datagnasker'),
-    PASSWORD=os.getenv('PASS', 'kodeord1'),
-    SECRET=uuid.uuid4()
-)
+
+
+def create_app():
+    app.config.update(
+            FLAG=os.getenv('FLAG', 'FLAG_PLACEHOLDER'),
+            USERNAME=os.getenv('NAME', 'datagnasker'),
+            PASSWORD=os.getenv('PASS', 'kodeord1'),
+            SECRET=uuid.uuid4()
+            )
+    return app
 
 import bitbibliotek.views
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from bitbibliotek import create_app
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = create_app()
